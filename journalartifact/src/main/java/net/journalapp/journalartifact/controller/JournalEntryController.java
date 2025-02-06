b397bebf7d70702e3b710bda28565f31282731f5
ashutosh <jhaashu09868@gmail.com>
2025-02-07 01:12:34 +0530

    Journal and User Entity Created using DBRef

--- journalartifact/src/main/java/net/journalapp/journalartifact/controller/JournalEntryController.java
+++ journalartifact/src/main/java/net/journalapp/journalartifact/controller/JournalEntryController.java
@@ -1,15 +1,21 @@
 package net.journalapp.journalartifact.controller;
 
 import net.journalapp.journalartifact.entity.JournalEntry;
+import net.journalapp.journalartifact.entity.User;
 import net.journalapp.journalartifact.service.JournalEntryService;
+import net.journalapp.journalartifact.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journal")
@@ -17,35 +23,61 @@ public class JournalEntryController {
 
     @Autowired
     private JournalEntryService journalEntryService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public List<JournalEntry> getAll(){
-        return  journalEntryService.getAll();
+    public ResponseEntity<List<JournalEntry>> getAllJournal(){
+        List<JournalEntry> all = journalEntryService.getAll();
+        return new ResponseEntity<>(all,HttpStatus.OK );
+    }
+    @GetMapping("/{username}")
+    public ResponseEntity<List<JournalEntry>> getAllJournalEntriesOfUser(@PathVariable String username){
+        User user = userService.findByUserName(username);
+        List<JournalEntry> journalEntries = user.getJournalEntries();
+        if(journalEntries != null && !journalEntries.isEmpty()){
+            return new ResponseEntity<List<JournalEntry>>(journalEntries ,HttpStatus.OK );
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND );
     }
 
-    @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+    @PostMapping("/{username}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry , @PathVariable String username){
+        try {
+             journalEntryService.saveEntry(myEntry , username);
+             return new ResponseEntity<>(myEntry , HttpStatus.OK);
+        }catch (Exception e){
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/id/{id}")
-    public JournalEntry getJournalByID(@PathVariable ObjectId id){
-
-        if(id != null)
-        return journalEntryService.getJournalByID(id);
-        else return null;
+    public ResponseEntity<?> getJournalEntryByID(@PathVariable ObjectId id){
+        JournalEntry journalEntry = journalEntryService.getJournalByID(id);
+        if(journalEntry != null){
+           return new ResponseEntity<>(journalEntry , HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/id/{id}")
-    public void deleteJournalByID(@PathVariable ObjectId id){
-        journalEntryService.deleteJournalByID(id);
+    @DeleteMapping("/id/{username}/{id}")
+    public ResponseEntity<?> deleteJournalByID(@PathVariable ObjectId id , @PathVariable String username){
+        journalEntryService.deleteJournalByID(id , username);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/id/{id}")
-    public void updateJournalByID(@PathVariable ObjectId id){
-        
+    @PutMapping("/id/{username}/{id}")
+    public ResponseEntity<?> updateJournalByID(@PathVariable ObjectId id , @RequestBody JournalEntry journalEntry , @PathVariable String username){
+        JournalEntry old = journalEntryService.getJournalByID(id);
+        if(old != null){
+           old.setTitle(journalEntry.getTitle()!=null && !journalEntry.getTitle().equals("")? journalEntry.getTitle() : old.getTitle());
+           old.setContent(journalEntry.getContent() != null && !journalEntry.getContent().equals("") ? journalEntry.getContent() : old.getContent());
+           journalEntryService.saveEntry(old);
+           return new ResponseEntity<>(old,HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
--- journalartifact/src/main/java/net/journalapp/journalartifact/service/JournalEntryService.java
+++ journalartifact/src/main/java/net/journalapp/journalartifact/service/JournalEntryService.java
@@ -1,12 +1,14 @@
 package net.journalapp.journalartifact.service;
 
-import net.journalapp.journalartifact.controller.JournalEntryController;
 import net.journalapp.journalartifact.entity.JournalEntry;
+import net.journalapp.journalartifact.entity.User;
 import net.journalapp.journalartifact.repository.JournalEntryRepository;
+import net.journalapp.journalartifact.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -15,6 +17,21 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry(JournalEntry journalEntry, String username){
+        User user = userService.findByUserName(username);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
+    //method overloading
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
     }
@@ -28,7 +45,10 @@ public class JournalEntryService {
                 .orElseThrow(() -> new RuntimeException("Journal Entry not found with ID: " + id));
     }
 
-    public void deleteJournalByID(ObjectId id){
+    public void deleteJournalByID(ObjectId id, String username){
+        User user = userService.findByUserName(username);
+        user.getJournalEntries().removeIf(x->x.getId().equals(id));
+        userRepository.save(user);
         journalEntryRepository.deleteById(id);
     }
 }
